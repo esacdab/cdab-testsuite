693109eba7bb3c97741f9cfe1da78369135a68d9
floeschau <frank.loeschau@terradue.com>
2021-12-09 17:57:28 +0000

    Chages for Amazon and Google (latency, TS06)

--- src/cdab-client/TestCases/TestCase601.cs
+++ src/cdab-client/TestCases/TestCase601.cs
@@ -18,15 +18,20 @@ for bechmarking various Copernicus Data Provider targets.
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Net;
 using cdabtesttools.Config;
 using cdabtesttools.Data;
 using cdabtesttools.Measurement;
 using cdabtesttools.Target;
 using log4net;
-using Terradue.Metadata.EarthObservation.OpenSearch.Extensions;
+using Terradue.OpenSearch;
 using Terradue.OpenSearch.Result;
+using Terradue.Metadata.EarthObservation.OpenSearch.Extensions;
 
 namespace cdabtesttools.TestCases
 {
@@ -37,7 +42,6 @@ namespace cdabtesttools.TestCases
         {
             Id = "TC601";
             Title = "Data Operational Latency Analysis [including Time Critical]";
-
         }
 
         public override void PrepareTest()
@@ -47,7 +51,6 @@ namespace cdabtesttools.TestCases
             // Get all Target Site catalogue sets configured as 'Baseline'
             foreach (var set in target.TargetSiteConfig.Data.Catalogue.Sets.Where(cs => cs.Value.Type == TargetCatalogueSetType.baseline))
             {
-                // Generate all Data Collection Filter Definition for the set
                 foreach (var division in DataHelper.GenerateDataOperationalLatencyFiltersDefinition(set.Key, set.Value))
                 {
                     queryFilters.Enqueue(division);
@@ -73,11 +76,257 @@ namespace cdabtesttools.TestCases
             }
         }
 
+
+        public override IEnumerable<TestUnitResult> RunTestUnits(TaskFactory taskFactory, Task prepTask)
+        {
+            // For providers that do not offer online dates, use special processing
+            if (target.TargetSiteConfig.Data.Catalogue.LatencyPolling) {
+                log.Info("Latencies calculated by polling the target catalogue; process may take some time");
+
+                if (StartTime.Ticks == 0)
+                    StartTime = DateTimeOffset.UtcNow;
+
+                if (prepTask.IsFaulted)
+                    throw prepTask.Exception;
+
+                List<Task<TestUnitResult>> _testUnits = new List<Task<TestUnitResult>>();
+                Task[] previousTask = Array.ConvertAll(new int[target.TargetSiteConfig.MaxCatalogueThread], mp => prepTask);
+
+                int i = queryFilters.Count();
+
+                while (i > 0)
+                {
+                    for (int j = 0; j < target.TargetSiteConfig.MaxCatalogueThread; j++)
+                    {
+                        var _testUnit = previousTask[j].ContinueWith<KeyValuePair<IOpenSearchable, FiltersDefinition>>((task) =>
+                        {
+                            prepTask.Wait();
+                            FiltersDefinition randomFilter;
+                            queryFilters.TryDequeue(out randomFilter);
+                            return new KeyValuePair<IOpenSearchable, FiltersDefinition>(target.CreateOpenSearchableEntity(randomFilter, Configuration.Current.Global.QueryTryNumber), randomFilter);
+                        }).
+                            ContinueWith((request) =>
+                            {
+                                if (request.Result.Value == null) return null;
+                                FiltersDefinition parameters = request.Result.Value;
+                                IOpenSearchable targetEntity = request.Result.Key;
+
+                                return PollUntilAvailable(targetEntity, parameters);
+                            });
+                        _testUnits.Add(_testUnit);
+                        previousTask[j] = _testUnit;
+                        i--;
+                    }
+                }
+                try
+                {
+                    Task.WaitAll(_testUnits.ToArray());
+                }
+                catch (AggregateException ae)
+                {
+                    Exception ex = ae;
+                    while (ex is AggregateException)
+                    {
+                        ex = ex.InnerException;
+                    }
+                    log.Debug(ex.Message);
+                    log.Debug(ex.StackTrace);
+                }
+                EndTime = DateTimeOffset.UtcNow;
+
+                return _testUnits.Select(t => t.Result).Where(r => r != null);
+
+            }
+
+            // For normal providers, use default (TC201) processing
+            return base.RunTestUnits(taskFactory, prepTask);
+            
+        }
+
+
+        public TestUnitResult PollUntilAvailable(IOpenSearchable targetEntity, FiltersDefinition fd)
+        {
+
+            List<IMetric> metrics = new List<IMetric>();
+
+            log.DebugFormat("[{1}] > Query {0} {2}...", fd.Name, Task.CurrentId, fd.Label);
+
+            DateTime fromTime = DateTime.UtcNow;
+            
+            var parameters = fd.GetNameValueCollection();
+
+            return catalogue_task_factory.StartNew(() =>
+            {
+                metrics.Add(new StringMetric(MetricName.dataCollectionDivision, fd.Label, "string"));
+                if (true)
+                {
+                    List<double> opsLatencies = new List<double>();
+                    long validatedResults = 0;
+
+                    int latencyCheckMaxDuration = target.TargetSiteConfig.Data.Catalogue.LatencyCheckMaxDuration;
+                    if (latencyCheckMaxDuration == 0) latencyCheckMaxDuration = 3600;
+                    int latencyCheckInterval = target.TargetSiteConfig.Data.Catalogue.LatencyCheckInterval;
+                    if (latencyCheckInterval == 0) latencyCheckInterval = 600;
+                    DateTime maxEndTime = DateTime.UtcNow.AddSeconds(latencyCheckMaxDuration);
+                    log.InfoFormat("Checking for new product in target until {0}", maxEndTime.ToString("yyyy-MM-ddTHH:mm:ssZ"));
+
+                    bool oneMissing = true;
+                    try {
+                        int iterationCount = 0;
+
+                        while (oneMissing && DateTime.UtcNow < maxEndTime) {
+                            iterationCount += 1;
+
+                            oneMissing = false;
+
+                            Stopwatch sw = new Stopwatch();
+                            DateTimeOffset timeStart = DateTimeOffset.UtcNow;
+                            IOpenSearchResultItem targetItem = null;
+                            IOpenSearchResultCollection targetResults = null;
+                            sw.Start();
+                            try
+                            {
+                                targetItem = FindNewItem(
+                                    targetEntity,
+                                    fd,
+                                    fromTime,
+                                    out targetResults
+                                );
+                            }
+                            catch (WebException we)
+                            {
+                                log.Warn("Error during target request new item");
+                                log.WarnFormat("Error message: {0}", we.Message);
+                            }
+                            var respTime = sw.ElapsedMilliseconds;
+                            sw.Stop();
+                            DateTimeOffset timeStop = DateTimeOffset.UtcNow;
+
+                            if (targetItem == null)
+                            {
+                                log.Info("No new item available yet in target");
+                                oneMissing = true;
+                            }
+                            else
+                            {
+                                long serializedSize = 0;
+                                try
+                                {
+                                    serializedSize = Encoding.Default.GetBytes(targetResults.SerializeToString()).Length;
+                                }
+                                catch
+                                {
+
+                                }
+                                var metricsArray = targetResults.ElementExtensions.ReadElementExtensions<Terradue.OpenSearch.Benchmarking.Metrics>("Metrics", "http://www.terradue.com/metrics", Terradue.OpenSearch.Benchmarking.MetricFactory.Serializer);
+                                if (metricsArray == null || metricsArray.Count() == 0)
+                                {
+                                    log.Warn("No query metrics found! Response Time and error rate may be biased!");
+                                    metrics.Add(new LongMetric(MetricName.responseTime, respTime, "ms"));
+                                    metrics.Add(new LongMetric(MetricName.size, serializedSize, "bytes"));
+                                    metrics.Add(new LongMetric(MetricName.beginGetResponseTime, timeStart.Ticks, "ticks"));
+                                    metrics.Add(new LongMetric(MetricName.endGetResponseTime, timeStop.Ticks, "ticks"));
+                                }
+                                else
+                                {
+                                    Terradue.OpenSearch.Benchmarking.Metrics osMetrics = metricsArray.First();
+                                    Terradue.OpenSearch.Benchmarking.Metric _sizeMetric = osMetrics.Metric.FirstOrDefault(m => m.Identifier == "size");
+                                    Terradue.OpenSearch.Benchmarking.Metric _responseTimeMetric = osMetrics.Metric.FirstOrDefault(m => m.Identifier == "responseTime");
+                                    Terradue.OpenSearch.Benchmarking.Metric _retryNumberMetric = osMetrics.Metric.FirstOrDefault(m => m.Identifier == "retryNumber");
+                                    Terradue.OpenSearch.Benchmarking.Metric _beginGetResponseTime = osMetrics.Metric.FirstOrDefault(m => m.Identifier == "beginGetResponseTime");
+                                    Terradue.OpenSearch.Benchmarking.Metric _endGetResponseTime = osMetrics.Metric.FirstOrDefault(m => m.Identifier == "endGetResponseTime");
+
+                                    log.DebugFormat("[{4}] < {0}/{1} entries for {6} {5}. {2}bytes in {3}ms", targetResults.Count, targetResults.TotalResults, _sizeMetric.Value, _responseTimeMetric.Value, Task.CurrentId, fd.Label, fd.Name);
+                                    if (_responseTimeMetric != null)
+                                    {
+                                        metrics.Add(new LongMetric(MetricName.responseTime, Convert.ToInt64(_responseTimeMetric.Value), "ms"));
+                                    }
+                                    else
+                                    {
+                                        metrics.Add(new LongMetric(MetricName.responseTime, respTime, "ms"));
+                                    }
+
+                                    if (_sizeMetric != null)
+                                        metrics.Add(new LongMetric(MetricName.size, Convert.ToInt64(_sizeMetric.Value), "bytes"));
+                                    else
+                                        metrics.Add(new LongMetric(MetricName.size, serializedSize, "bytes"));
+
+                                    if (_retryNumberMetric != null)
+                                        metrics.Add(new LongMetric(MetricName.retryNumber, Convert.ToInt64(_retryNumberMetric.Value), "#"));
+                                    else
+                                        metrics.Add(new LongMetric(MetricName.retryNumber, 1, "#"));
+
+                                    if (_beginGetResponseTime != null && _endGetResponseTime != null)
+                                    {
+                                        metrics.Add(new LongMetric(MetricName.beginGetResponseTime, Convert.ToInt64(_beginGetResponseTime.Value), "ticks"));
+                                        metrics.Add(new LongMetric(MetricName.endGetResponseTime, Convert.ToInt64(_endGetResponseTime.Value), "ticks"));
+                                    }
+                                    else
+                                    {
+                                        metrics.Add(new LongMetric(MetricName.beginGetResponseTime, timeStart.Ticks, "ticks"));
+                                        metrics.Add(new LongMetric(MetricName.endGetResponseTime, timeStop.Ticks, "ticks"));
+                                    }
+                                }
+
+                                DateTimeOffset creationDate = DateTimeOffset.UtcNow;
+                                log.DebugFormat("Target item estimated creation date ({0}): {1}", targetItem.Id, creationDate.ToString("yyyy-MM-ddTHH:mm:ssZ"));
+
+                                DateTimeOffset measurementDate = targetItem.FindStartDate();
+                                log.DebugFormat("Item measurement date ({0}): {1}", targetItem.Id, measurementDate.ToString("yyyy-MM-ddTHH:mm:ssZ"));
+
+                                double latencySeconds = creationDate.Subtract(measurementDate).TotalSeconds;
+                                log.DebugFormat("Latency (seconds) ({0}): {1}", targetItem.Id, latencySeconds);
+
+                                opsLatencies.Add(latencySeconds);
+                                validatedResults++;
+                            }
+
+                            if (oneMissing) {
+                                log.InfoFormat("Sleep for {0} seconds", latencyCheckInterval);
+                                Thread.Sleep(latencyCheckInterval * 1000);
+                            }
+                        }
+
+                        if (oneMissing)
+                        {
+                            log.WarnFormat("Target item not online within time limit");
+                        }
+
+                        metrics.Add(new LongMetric(MetricName.wrongResultsCount, 1 - validatedResults, "#"));
+                        metrics.Add(new LongMetric(MetricName.totalValidatedResults, validatedResults, "#"));
+                        metrics.Add(new LongMetric(MetricName.totalReadResults, 1, "#"));
+                        
+                        if (opsLatencies.Count() > 0)
+                        {
+                            metrics.Add(new LongMetric(MetricName.avgDataOperationalLatency, (long)opsLatencies.Average(), "sec"));
+                            metrics.Add(new LongMetric(MetricName.maxDataOperationalLatency, (long)opsLatencies.Max(), "sec"));
+                        }
+                        else
+                        {
+                            metrics.Add(new LongMetric(MetricName.avgDataOperationalLatency, -1, "sec"));
+                            metrics.Add(new LongMetric(MetricName.maxDataOperationalLatency, -1, "sec"));
+                        }
+                        //metrics.Add(new LongMetric(MetricName.analysisTime, sw3.ElapsedMilliseconds, "msec"));
+                    }
+                    catch (Exception e)
+                    {
+                        metrics.Add(new ExceptionMetric(e));
+                        log.ErrorFormat("[{0}] < Analysis failed for results {2} : {1}", Task.CurrentId, e.Message, fd.Label);
+                        log.Debug(e.StackTrace);
+                    }
+
+                }
+
+                return new TestUnitResult(metrics, fd);
+            }).Result;
+        }        
+
+
         protected override IEnumerable<IMetric> AnalyzeResults(IOpenSearchResultCollection results, FiltersDefinition fd)
         {
             List<IMetric> metrics = new List<IMetric>();
-            long wrongResults = 0;
             long validatedResults = 0;
+            long wrongResults = 0;
 
             log.DebugFormat("[{1}] Validating and Analyzing {0} result items...", results.Items.Count(), Task.CurrentId);
 
@@ -111,10 +360,24 @@ namespace cdabtesttools.TestCases
                         wrongResults++;
                     }
                 }
-                opsLatencies.Add(item.PublishDate.Subtract(item.FindStartDate()).TotalSeconds);
+                DateTimeOffset creationDate;
+                creationDate = item.PublishDate;
+                var dateStrings = item.ElementExtensions.ReadElementExtensions<string>("creationDate", "http://www.terradue.com/");
+                if (dateStrings != null && dateStrings.Count() > 0)
+                {
+                    DateTimeOffset.TryParse(dateStrings.FirstOrDefault(), System.Globalization.CultureInfo.InstalledUICulture, System.Globalization.DateTimeStyles.AssumeUniversal, out creationDate);
+                }
+                log.DebugFormat("Target item creation date ({0}): {1}", item.Id, creationDate.ToString("yyyy-MM-ddTHH:mm:ssZ"));
+                
+                DateTimeOffset measurementDate = item.FindStartDate();
+                log.DebugFormat("Item measurement date ({0}): {1}", item.Id, measurementDate.ToString("yyyy-MM-ddTHH:mm:ssZ"));
+
+                double latencySeconds = creationDate.Subtract(measurementDate).TotalSeconds;
+                log.DebugFormat("Latency (seconds) ({0}): {1}", item.Id, latencySeconds);
+
+                opsLatencies.Add(latencySeconds);
                 validatedResults++;
             }
-
 
             metrics.Add(new LongMetric(MetricName.wrongResultsCount, wrongResults, "#"));
             metrics.Add(new LongMetric(MetricName.totalValidatedResults, validatedResults, "#"));
@@ -129,8 +392,21 @@ namespace cdabtesttools.TestCases
                 metrics.Add(new LongMetric(MetricName.avgDataOperationalLatency, -1, "sec"));
                 metrics.Add(new LongMetric(MetricName.maxDataOperationalLatency, -1, "sec"));
             }
-
             return metrics;
+        }
+
+
+        private IOpenSearchResultItem FindNewItem(IOpenSearchable os, FiltersDefinition filters, DateTime fromTime, out IOpenSearchResultCollection result)
+        {
+            if (filters == null) filters = new FiltersDefinition("new_product_search");
+            DateTime startTime = fromTime;
+            DateTime endTime = fromTime.AddDays(30);
+            if (endTime == startTime) endTime = endTime.AddDays(1);
+            filters.AddFilter("start", "{http://a9.com/-/opensearch/extensions/time/1.0/}start", startTime.ToString("yyyy-MM-dd\\THH:mm:ss.fff\\Z"), startTime.ToString("O"), null, null);
+            filters.AddFilter("stop", "{http://a9.com/-/opensearch/extensions/time/1.0/}end", endTime.ToString("yyyy-MM-dd\\THH:mm:ss.fff\\Z"), endTime.ToString("O"), null, null);
+
+            result = ose.Query(os, filters.GetNameValueCollection());
+            return result.Items.FirstOrDefault();
         }
     }
 }
