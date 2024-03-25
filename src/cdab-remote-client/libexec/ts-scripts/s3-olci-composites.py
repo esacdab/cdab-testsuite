049e07c5cf40f68db4d0238e10be41dfc31897b0
Emmanuel Mathot <emmanuel.mathot@terradue.com>
2024-03-25 21:58:55 +0100

    Add GraphProcessor class and methods to create and run SNAP Graph

--- src/cdab-remote-client/libexec/ts-scripts/s3-olci-composites.py
+++ src/cdab-remote-client/libexec/ts-scripts/s3-olci-composites.py
@@ -18,8 +18,204 @@ from shapely.geometry import box
 import shutil
 import math
 import json
+import lxml.etree as etree
+import subprocess
+import tempfile
 import snappy 
 from snappy import GPF
+
+class GraphProcessor():
+    """SNAP Graph class
+
+    This class provides the methods to create, view and run a SNAP Graph
+
+    Attributes:
+        None.
+    """
+    
+    def __init__(self, wdir='.'):
+        self.root = etree.Element('graph')
+    
+        version = etree.SubElement(self.root, 'version')
+        version.text = '1.0'
+        self.pid = None
+        self.p = None
+        self.wdir = wdir
+   
+    def view_graph(self):
+        """This method prints SNAP Graph
+    
+        Args:
+            None.
+
+        Returns
+            None.
+
+        Raises:
+            None.
+        """
+        print(etree.tostring(self.root , pretty_print=True))
+        
+    def add_node(self, node_id, operator, parameters, source):
+        """This method adds or overwrites a node to the SNAP Graph
+    
+        Args:
+            node_id: node identifier
+            operator: SNAP operator
+            parameter: dictionary with the SNAP operator parameters
+            source: string or list of sources (previous node identifiers in the SNAP Graph)
+
+        Returns
+            None.
+
+        Raises:
+            None.
+        """
+        xpath_expr = '/graph/node[@id="%s"]' % node_id
+
+        if len(self.root.xpath(xpath_expr)) != 0:
+
+            node_elem = self.root.xpath(xpath_expr)[0]
+            operator_elem = self.root.xpath(xpath_expr + '/operator')[0]
+            sources_elem = self.root.xpath(xpath_expr + '/sources')[0]
+            parameters_elem = self.root.xpath(xpath_expr + '/parameters')
+
+            for key, value in parameters.iteritems():
+                
+                if key == 'targetBandDescriptors':
+                                        
+                    parameters_elem.append(etree.fromstring(value))
+                    
+                else:
+                    p_elem = self.root.xpath(xpath_expr + '/parameters/%s' % key)[0]
+
+                    if value is not None:             
+                        if value[0] != '<':
+                            p_elem.text = value
+                        else:
+                            p_elem.text.append(etree.fromstring(value))
+    
+        else:
+
+            node_elem = etree.SubElement(self.root, 'node')
+            operator_elem = etree.SubElement(node_elem, 'operator')
+            sources_elem = etree.SubElement(node_elem, 'sources')
+
+            if isinstance(source, list):
+
+                for index, s in enumerate(source):
+                    if index == 0:  
+                        source_product_elem = etree.SubElement(sources_elem, 'sourceProduct')
+
+                    else: 
+                        source_product_elem = etree.SubElement(sources_elem, 'sourceProduct.%s' % str(index))
+
+                    source_product_elem.attrib['refid'] = s
+            
+            elif isinstance(source, dict):
+
+                for key, value in source.iteritems():
+                    
+                    source_product_elem = etree.SubElement(sources_elem, key)
+                    source_product_elem.text = value
+            
+            elif source != '':
+                source_product_elem = etree.SubElement(sources_elem, 'sourceProduct')
+                source_product_elem.attrib['refid'] = source
+
+            parameters_elem = etree.SubElement(node_elem, 'parameters')
+            parameters_elem.attrib['class'] = 'com.bc.ceres.binding.dom.XppDomElement'
+
+            for key, value in parameters.iteritems():
+
+                if key == 'targetBandDescriptors':
+                                        
+                    parameters_elem.append(etree.fromstring(value))
+                    
+                else:
+                
+                    parameter_elem = etree.SubElement(parameters_elem, key)
+
+                    if value is not None:             
+                        if value[0] != '<':
+                            parameter_elem.text = value
+                        else:
+                            parameter_elem.append(etree.fromstring(value))
+
+        node_elem.attrib['id'] = node_id
+
+        operator_elem.text = operator 
+
+    def save_graph(self, filename):
+        """This method saves the SNAP Graph
+    
+        Args:
+            filename: XML filename with '.xml' extension
+
+        Returns
+            None.
+
+        Raises:
+            None.
+        """
+        with open(filename, 'wb') as file:
+            file.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+            file.write(etree.tostring(self.root, pretty_print=True))
+     
+    #def plot_graph(self):
+        
+    #    for node_id in self.root.xpath('/graph/node/@id'):
+            
+
+    #        xpath_expr = '/graph/node[@id="%s"]' % node_id
+            
+    #        if len(self.root.xpath(xpath_expr + '/sources/sourceProduct')) != 0:
+    #            print(self.root.xpath(xpath_expr + '/sources/sourceProduct'))[0].attrib['refid']
+    #            print(node_id)
+    #        else:
+    #            print(node_id)
+    #    return True
+    
+    def run(self):
+        """This method runs the SNAP Graph using gpt
+    
+        Args:
+            None.
+
+        Returns
+            res: gpt exit code 
+            err: gpt stderr
+
+        Raises:
+            None.
+        """
+        os.environ['LD_LIBRARY_PATH'] = '.'
+        
+        print('Processing the graph')
+        
+        fd, path = tempfile.mkstemp()
+        
+        try:
+        
+            self.save_graph(filename=path)
+            options = ['/opt/snap/bin/gpt',
+               '-x',
+               '-c',
+               '2048M',
+               path]
+
+            p = subprocess.Popen(options,
+                stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+
+            print('Process PID: %s' % p.pid)
+            res, err = p.communicate()
+            print (res, err)
+        finally:
+            os.remove(path)
+            
+            print('Done.')
+        return res, err
+
 
 def get_snap_parameters(operator):
     """This function returns the SNAP operator ParameterDescriptors (snappy method op_spi.getOperatorDescriptor().getParameterDescriptors())
@@ -283,7 +479,21 @@ def s3_olci_import(idepix, **kwargs):
         print('Updating Operator {}'.format(key))
         options[key.replace('_', '-')].update(value)
      
-    print('Adding Operator {} to graph'.format(operator))
+    mygraph = GraphProcessor()
+    
+    for index, operator in enumerate(operators):
+        print ('Adding Operator {} to graph').format(operator)
+        if index == 0:            
+            source_node_id = ''
+        else:
+            source_node_id = operators[index - 1]
+       
+        if operator == 'Idepix.Sentinel3.Olci':
+            mygraph.add_node(operator, operator, idepix, source_node_id)
+        else:
+            mygraph.add_node(operator, operator, options[operator], source_node_id)
+    
+    mygraph.run()
     
     
 def s3_rgb_composite(red, green, blue, classif_flags, geo_transform, projection_ref, output_name):
